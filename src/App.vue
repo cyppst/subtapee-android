ff0e0d4fd9e75f63223bf3197cdef3fcfea9d86a
golfcomsci <golfcomsci57@gmail.com>
2018-09-22 03:45:13 +0700

    dd

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,21 @@
 <script>
   export default {
     name: 'App',
-    components: {
-    },
+    components: {},
 
     created () {
-        const token = window.localStorage['token'] || '';
+      const token = window.localStorage['token'] || ''
       if (token) {
         this.$setAxiosHeader(token)
+        this.$axiosInstance.interceptors.response.use(undefined, function (err) {
+          return new Promise(function (resolve, reject) {
+            if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+              this.$store.dispatch('auth/logout')
+              this.$router.push('/login')
+            }
+            throw err
+          })
+        })
       }
     }
   }
